b4bcce0280d6cde55363d9554e7729e0578270a5
TaeWoongKim2 <ktaewoong46@gmail.com>
2022-08-08 20:00:52 +0900

    refector: ESLint 설정에 따른 파일 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'naver',
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["plugin:react/recommended", "naver"],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		"object-curly-spacing": ["error", "always", { "objectsInObjects": false }],
+	},
 };
