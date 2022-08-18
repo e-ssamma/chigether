34da6dc43d6beb6ef99f6341906386ad2955703d
ssamma1 <159houng@naver.com>
2022-08-18 17:06:08 +0900

    add header, sidebar, footer

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import logo from "logo.svg";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import Footer from "./Footer";
 
 const Content = () => (
 	<>
-		<img src={logo} className="App-logo" alt="logo" />
-		<p>
-          Edit
-			{" "}
-			<code>src/App.js</code>
-			{" "}
-          and save to reload.
-		</p>
-		<a
-			className="App-link"
-			href="https://reactjs.org"
-			target="_blank"
-			rel="noopener noreferrer"
-		>
-          Learn React
-		</a>
+		<Header />
+		<SideBar />
+		<Footer />
 	</>
 );
 
