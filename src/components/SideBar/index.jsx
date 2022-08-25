e1bd8c475cabc89d4748e92e686a6fe71222ca90
ssamma1 <159houng@naver.com>
2022-08-25 18:48:41 +0900

    css개선1, html5 tag 적용

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,42 +1,43 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import "./style.css";
 
 function SideBar() {
   return (
-    <>
+    <nav className="sidebar">
       <Link to="/Allpage">
-	    <button>전체글 보기</button>
+	    <button className="sidebar-button">전체글 보기</button>
+        <br></br>
+      </Link>
+	  <Link to="/TopChicken">
+	    <button className="sidebar-button">TOP치킨</button>
+        <br></br>
+      </Link>
+	  <Link to="/Bestpage">
+	    <button className="sidebar-button">인기게시판</button>
         <br></br>
       </Link>
 	  <Link to="/Allpage">
-	    <button>TOP치킨</button>
+	    <button className="sidebar-button">인기글</button>
         <br></br>
       </Link>
 	  <Link to="/Allpage">
-	    <button>인기게시판</button>
+	    <button className="sidebar-button">치킨홍보</button>
         <br></br>
       </Link>
 	  <Link to="/Allpage">
-	    <button>인기글</button>
+	    <button className="sidebar-button">치킨Q&A</button>
         <br></br>
       </Link>
 	  <Link to="/Allpage">
-	    <button>치킨홍보</button>
+	    <button className="sidebar-button">치킨TIP</button>
         <br></br>
       </Link>
 	  <Link to="/Allpage">
-	    <button>치킨Q&A</button>
+	    <button className="sidebar-button">치킨창업</button>
         <br></br>
       </Link>
-	  <Link to="/Allpage">
-	    <button>치킨TIP</button>
-        <br></br>
-      </Link>
-	  <Link to="/Allpage">
-	    <button>치킨창업</button>
-        <br></br>
-      </Link>
-    </>
+    </nav>
   );
 }
 
