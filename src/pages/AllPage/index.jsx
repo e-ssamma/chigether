95daa4b9d2b859d45e5cb9bcab4122806f42d211
ssamma1 <159houng@naver.com>
2022-09-13 17:55:31 +0900

    전체게시판 페이지 작업1 전체 페이지 content 분류 list 추가

--- src/pages/AllPage/index.jsx
+++ src/pages/AllPage/index.jsx
@@ -4,8 +4,40 @@ import styled from "styled-components";
 function allpage() {
   return (
     <Container className="container">
-      <div>
-        <h1>안녕? 난 전체 페이지야</h1>
+      <div className="brand--box">		
+        <h3 className="brand--title">교촌치킨</h3>
+        <ul className="brand--menu">
+          <li>교촌 오리지날</li>
+          <li>교촌 허니콤보</li>
+          <li>교촌 레드콤보</li>
+          <li>교촌 블랙시크릿</li>
+          <li>교촌 리얼후라이드</li>
+          <li>교촌 치즈트러플</li>
+        </ul>
+      </div>
+	  <div className="brand--box">		
+        <h3 className="brand--title">굽네치킨</h3>
+      </div>
+	  <div className="brand--box">		
+        <h3 className="brand--title">BBQ치킨</h3>
+      </div>
+      <div className="brand--box">		
+        <h3 className="brand--title">맘스터치</h3>
+      </div>
+      <div className="brand--box">		
+        <h3 className="brand--title">노랑통닭</h3>
+      </div>
+      <div className="brand--box">		
+        <h3 className="brand--title">60계치킨</h3>
+      </div>
+      <div className="brand--box">		
+        <h3 className="brand--title">네네치킨</h3>
+      </div>
+      <div className="brand--box">		
+        <h3 className="brand--title">BHC치킨</h3>
+      </div>
+      <div className="brand--box">		
+        <h3 className="brand--title">빈칸</h3>
       </div>
 	    
     </Container>
@@ -19,13 +51,8 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-between;
 
-  .content--box {
-    flex: 1 1 48%;
+  .brand--box {
+    flex: 1 1 30%;
     border: 1px solid #333;
-  }
-
-  .content--box:nth-child(5),
-  .content--box:nth-child(8) {
-    flex: 1 1 100%;
   }
 `;
